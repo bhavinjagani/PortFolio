1dc5ba2ee90052fad780cf28b2e1239b3fc23da4
devbhavinpatel <bjagani@girsoftwareservices.com>
2025-02-11 17:52:09 -0600

    add cpde

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import linkedin from "./assets/linkedin-dark.svg";
 import github from "./assets/github-dark.svg";
 import { FiMenu, FiX } from "react-icons/fi";
 import Typewriter from "typewriter-effect";
-
+import "./App.css";
 export default function App() {
   const [showPopup, setShowPopup] = useState(false);
   const [popupContent, setPopupContent] = useState("");
@@ -22,6 +22,9 @@ export default function App() {
     setPopupLink(link);
     setShowPopup(true);
   };
+  function isMobile() {
+    return window.innerWidth < 768;
+  }
   return (
     <div className=" text-gray-900">
       <nav className="p-4 flex justify-between items-center bg-white fixed w-full z-10 shadow-lg">
@@ -79,6 +82,7 @@ export default function App() {
               <span className="px-4 py-2 mt-1 mb-1 bg-gray-300  rounded">React.js</span>
               <span className="px-4 py-2 mt-1 mb-1 bg-gray-300  rounded">Angular.js</span>
               <span className="px-4 py-2 mt-1 mb-1 bg-gray-300  rounded">Express.js</span>
+              <span className="px-4 py-2 mt-1 mb-1 bg-gray-300  rounded">Node.js</span>
               <span className="px-4 py-2 mt-1 mb-1 bg-gray-300  rounded">MySql</span>
               <span className="px-4 py-2 mt-1 mb-1 bg-gray-300  rounded">MongoDB</span>
               <span className="px-4 py-2 mt-1 mb-1 bg-gray-300  rounded">SEO</span>
@@ -88,7 +92,7 @@ export default function App() {
             </div>
           </section>
         </div>
-        <div className="fixed z-10 text-center top-1/3 left-0 p-2 md:p-4  rounded-md   bg-white">
+        <div className="fixed  text-center top-1/3 left-0 p-2 md:p-4  rounded-md   bg-white">
           <ul className="flex flex-col gap-3 ">
             <li className="self-center">
               <a href="mailto:bhavinjagani2001@gmail.com" ><img src={email} alt="Logo" className="h-6 w-6" /></a>
@@ -104,15 +108,15 @@ export default function App() {
         <section id="experience" className="py-20">
           <h2 className="text-3xl font-bold text-center">Experience</h2>
           <div className="mt-6 space-y-4 max-w-3xl mx-auto">
-            <motion.div whileHover={{ scale: 1.05 }} className="p-4 bg-gray-200 dark:bg-gray-800 rounded shadow cursor-pointer relative" onClick={() => handlePopup("GIR Software Services - Oracle NetSuite Alliance Partner", "https://girsoftwareservices.com/", " • Crafted a strategic solution for managing pre-order and back-order inventory that established clear visibility of stock levels, resulting in a 30% improvement in order accuracy and enhanced customer experience metrics.\n • Enhanced the performance of existing web pages by employing HTML, CSS, and JavaScript to optimize them for mobile devices; directly resulted in a 40% increase in mobile user interactions within one-month post-launch. \n • Streamlined the checkout process, reducing cart abandonment by 25% and boosting transaction completion rates, leading to increased sales over three months.\n • Implemented custom tags and functions to track product details in Google Analytics, enabling detailed performance analysis.\n • Developed a unique multi-step 'Gift Box Builder' tool on the e-commerce site, attracting more than 500 monthly users while enhancing personalized gifting experiences and elevating overall user interaction rates by approximately 30%.")}>
-              <h3 className="font-semibold">Web Developer - Get It Right Software Services</h3>
+            <motion.div whileHover={{ scale: 1.05 }} className="p-4 bg-gray-200 rounded shadow cursor-pointer relative" onClick={() => handlePopup("GIR Software Services - Oracle NetSuite Alliance Partner", "https://girsoftwareservices.com/", " • Crafted a strategic solution for managing pre-order and back-order inventory that established clear visibility of stock levels, resulting in a 30% improvement in order accuracy and enhanced customer experience metrics.\n • Enhanced the performance of existing web pages by employing HTML, CSS, and JavaScript to optimize them for mobile devices; directly resulted in a 40% increase in mobile user interactions within one-month post-launch. \n • Streamlined the checkout process, reducing cart abandonment by 25% and boosting transaction completion rates, leading to increased sales over three months.\n • Implemented custom tags and functions to track product details in Google Analytics, enabling detailed performance analysis.\n • Developed a unique multi-step 'Gift Box Builder' tool on the e-commerce site, attracting more than 500 monthly users while enhancing personalized gifting experiences and elevating overall user interaction rates by approximately 30%.")}>
+              <h3 className="font-semibold h-12">Web Developer - Get It Right Software Services</h3>
               <p className="text-sm">May 2022 - July 2024</p>
-              <button className="px-1 py-2 mt-1 mb-1 bg-gray-300 absolute rounded right-4 bottom-4">More Details</button>
+              <button className="px-1 py-2 mt-1 mb-1 bg-gray-300 absolute rounded right-4 bottom-1">More Details</button>
             </motion.div>
-            <motion.div whileHover={{ scale: 1.05 }} className="p-4 bg-gray-200 dark:bg-gray-800 rounded shadow cursor-pointer relative" onClick={() => handlePopup("WiseDV Electronics", "https://wisedv.com/", " • Developed and implemented a dynamic form frontend inspired by Google Forms using modern web technologies, resulting in improved usability and streamlined data collection. \n • Built a robust connection between the frontend and a MySQL database, optimizing storage and retrieval for over 1,500 monthly form submissions.\n • Developed RESTful APIs for seamless data interaction between frontend and backend systems, improving operational efficiency.")}>
-              <h3 className="font-semibold">Web Developer Intern - WiseDV Electronics</h3>
+            <motion.div whileHover={{ scale: 1.05 }} className="p-4 bg-gray-200 rounded shadow cursor-pointer relative" onClick={() => handlePopup("WiseDV Electronics", "https://wisedv.com/", " • Developed and implemented a dynamic form frontend inspired by Google Forms using modern web technologies, resulting in improved usability and streamlined data collection. \n • Built a robust connection between the frontend and a MySQL database, optimizing storage and retrieval for over 1,500 monthly form submissions.\n • Developed RESTful APIs for seamless data interaction between frontend and backend systems, improving operational efficiency.")}>
+              <h3 className="font-semibold h-12">Web Developer Intern - WiseDV Electronics</h3>
               <p className="text-sm">Dec 2021 - Mar 2022</p>
-              <button className="px-1 py-2 mt-1 mb-1 bg-gray-300 absolute rounded right-4 bottom-4">More Details</button>
+              <button className="px-1 py-2 mt-1 mb-1 bg-gray-300 absolute rounded right-4 bottom-1">More Details</button>
             </motion.div>
           </div>
         </section>
@@ -120,12 +124,12 @@ export default function App() {
         <section id="projects" className="py-20 text-center">
           <h2 className="text-3xl font-bold">Projects</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-6 max-w-4xl mx-auto">
-            <motion.div whileHover={{ scale: 1.05 }} className="p-4 bg-white dark:bg-gray-800 rounded shadow">
+            <motion.div whileHover={{ scale: 1.05 }} className="p-4 bg-white  rounded shadow">
               <h2 className="font-semibold">OLX Replica – MERN Stack</h2>
               <p className="font-semibold">Tools: MongoDB,React.js,Node.js,Tailwindcss</p>
               <p className="text-sm">Developed a full-stack application for buying and selling pre-owned products, mirroring OLX's functionality</p>
             </motion.div>
-            <motion.div whileHover={{ scale: 1.05 }} className="p-4 bg-white dark:bg-gray-800 rounded shadow">
+            <motion.div whileHover={{ scale: 1.05 }} className="p-4 bg-white  rounded shadow">
               <h2 className="font-semibold">Google Forms Clone</h2>
               <p className="font-semibold">Tools: Angular.js,Node.js, MySql , Bootstrap</p>
               <p className="text-sm">Developed and implemented a dynamic form frontend inspired by Google Forms using modern web technologies.</p>
@@ -152,11 +156,11 @@ export default function App() {
       </div>
 
       {showPopup && (
-        <motion.div  animate={{ scale: 1.3 }}
+        <motion.div animate={isMobile() ? { scale: 1 } : {scale:1.3}}
         whileInView={{ opacity: 1 }} className="fixed z-20 inset-0 bg-black bg-opacity-50 flex justify-center items-center">
-          <div className="bg-white dark:bg-gray-800 p-6 rounded shadow-lg max-w-2xl w-full relative">
+          <div className="bg-white p-6 rounded shadow-lg max-w-2xl w-full relative">
             <button className="absolute top-2 right-2  text-2xl bg-black text-white" onClick={() => setShowPopup(false)}><FiX /></button>
-            <h3 className="text-xl font-semibold mb-2 text-blue-500 underline"> <a href={popupLink} className="text-blue-500\" target="_blank\">{popupTitle}</a></h3>
+            <h3 className="text-base md:text-xl font-semibold mb-2 text-blue-500 underline"> <a href={popupLink} className="text-blue-500\" target="_blank\">{popupTitle}</a></h3>
             <p className="whitespace-pre-wrap font-bold text-sm">Work highlights</p>
             <p className="whitespace-pre-wrap text-sm">{popupContent}</p>
           </div>
